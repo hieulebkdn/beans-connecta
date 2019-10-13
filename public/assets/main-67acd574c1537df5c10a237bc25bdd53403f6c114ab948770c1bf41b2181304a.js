b7b13f9c7e752f247787dcb597c3a24484d71cc7
Trong Hieu Le <hieulebkdn@gmail.com>
2019-10-13 20:40:16 +0700

    index companies

--- public/assets/main-67acd574c1537df5c10a237bc25bdd53403f6c114ab948770c1bf41b2181304a.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    "use strict";
-    var $main_window = $(window);
-    $main_window.on("load", function() { $("#preloader").fadeOut("slow"); });
-    $main_window.on("scroll", function() { if ($(this).scrollTop() > 250) { $(".back-to-top").fadeIn(200); } else { $(".back-to-top").fadeOut(200); } });
-    $(".back-to-top").on("click", function() { $("html, body").animate({ scrollTop: 0 }, "slow"); return false; });
-    var logo_path = $('.mobile-menu').data('logo');
-    $('#main-navbar').slicknav({ appendTo: '.mobile-menu', removeClasses: false, label: '', closedSymbol: '<i class="lni-chevron-right"><i/>', openedSymbol: '<i class="lni-chevron-down"><i/>', brand: '<a href="index.html"><img src="' + logo_path + '" class="img-responsive" alt="logo"></a>' });
-})(jQuery);
