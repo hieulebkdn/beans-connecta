e006f3e914ceb637f6fc65fa16013ad1e45e9ddf
Trong Hieu Le <hieulebkdn@gmail.com>
2019-11-06 20:34:03 +0700

    ajax on update experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,12 +1,5 @@
 class ExperiencesController < ApplicationController
-    before_action :set_experience, only: %i(show edit destroy)
-
-    def index
-      @experiences = Experience.all
-    end
-
-    def show
-    end 
+    before_action :set_experience, only: %i(edit update destroy)
 
     def new
         @experience =Experience.new
@@ -16,13 +9,24 @@ class ExperiencesController < ApplicationController
         @experience = Experience.new experience_params
         if @experience.save
             flash[:success] = "Your experience profile successfully created!"
-            redirect_to candidate_edit_experience_path(current_user.profile)
+            redirect_to experience_edit_experience_path(current_user.profile)
         else
             render :new
         end
     end
 
     def edit
+    end
+    
+    def update
+        respond_to do |format|
+            if @experience.update_attributes(experience_params)
+              format.html { redirect_to candidate_edit_experience_path(current_user.profile), notice: "Experience was successfuly udate."}
+            else
+              format.html { redirect_to experience_edit_experience_path(current_user.profile), notice: "Experience was unsuccessfuly update."}
+              format.json { render json: @experience.errors, status: :unprocessable_entity }
+            end
+          end
     end
 
     def destroy
@@ -41,6 +45,6 @@ class ExperiencesController < ApplicationController
     end
 
     def experience_params
-        params.require(:experience).permit(:company_name, :position, :description, :candidate_id, :first_day, :last_day)
+        params.require(:experience).permit(:company_name, :position, :description, :experience_id, :first_day, :last_day)
     end
 end
